8f71ca9c5a176ecfcb07e0ee3e5c7d3753e986c6
Enu Azubike <enuazubike88@gmail.com>
2023-05-08 02:01:37 +0100

    created the user model

---
+++ nodejs_with_sequelize/basic-auth/src/models/User.js
@@ -0,0 +1,79 @@
+import '../config';
+
+import { Model, DataTypes } from 'sequelize';
+
+import bcrypt from 'bcrypt';
+
+import environment from '../config/env';
+
+export default (sequelize) => {
+  class User extends Model {
+    static associate(models) {}
+
+    static async hashPassword(password) {
+      return bcrypt.hash(password, environment.saltRounds);
+    }
+
+    static async comparePassword(password, hashedPassword) {
+      return bcrypt.compare(password, hashedPassword);
+    }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: {
+            msg: 'not a valid email address',
+          },
+        },
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      username: {
+        type: DataTypes.STRING(50),
+        unique: true,
+      },
+
+      firstName: {
+        type: DataTypes.STRING(50),
+        validate: {
+          len: {
+            args: [0, 50],
+            msg: 'first name must have characters between 0-50',
+          },
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING(50),
+        validate: {
+          len: {
+            args: [0, 50],
+            msg: 'last name must have characters between 0-50',
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      indexes: [
+        {
+          unique: true,
+          fields: ['email'],
+        },
+      ],
+    }
+  );
+  User.beforeSave(async (user, _) => {
+    const hashedPassword = await User.hashPassword(user.password);
+    user.password = hashedPassword;
+  });
+
+  return User;
+};
